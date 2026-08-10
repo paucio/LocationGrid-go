0ada87240ab63e6b2f6db7873bca953ca8b044d9
Paul Ciocan <paul.ciocan@192.168.1.6>
2026-08-10 10:57:52 +0300

    Added database connection

--- internal/model/point.go
+++ internal/model/point.go
@@ -3,7 +3,7 @@ package model
 // Point represents a point in a 2D space with an ID, name, and coordinates (X, Y).
 type Point struct {
 	ID int64 `json:"id"`
-	name string `json:"name"`
+	Name string `json:"name"`
 	X float64 `json:"x"`
 	Y float64 `json:"y"`	
 }
